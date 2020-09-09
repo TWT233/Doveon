a11cf978440ae2af4698842f71bd9514e94e5378
TWT <twt2333@outlook.com>
2020-09-10 00:20:30 +0800

    [add] StatusView.vue: to show status of current build

---
+++ src/components/build/StatusView.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-simple-table>
+    <template v-slot:default>
+      <tbody>
+        <tr v-for="item in statusKeys" :key="item">
+          <td>{{ item }}</td>
+          <td>{{ Math.floor(status[item]) }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { Status } from "@/mechanism/build/Status";
+
+@Component({})
+export default class StatusView extends Vue {
+  @Prop(Status) status!: Status;
+
+  get statusKeys(): string[] {
+    return Object.keys(this.status);
+  }
+}
+</script>
+
+<style scoped></style>
