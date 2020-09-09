78b580892685c8ee75c6646195e02a777f888df4
TWT <twt2333@outlook.com>
2020-09-10 00:21:17 +0800

    [add] Builder.vue: modify/show a set of build

---
+++ src/components/build/Builder.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="4">
+        <StatusView :status="build.status"></StatusView>
+      </v-col>
+      <v-col>
+        <v-container>
+          <v-row>
+            <v-col cols="12">
+              <GearSelect :gears="gears"></GearSelect>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12"> </v-col>
+          </v-row>
+        </v-container>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import GearSelect from "@/components/build/GearSelect.vue";
+import StatusView from "@/components/build/StatusView.vue";
+import { Gear } from "@/mechanism/build/Gear";
+import { Build } from "@/mechanism/build/Build";
+
+@Component({ components: { GearSelect, StatusView } })
+export default class Builder extends Vue {
+  build: Build = new Build();
+
+  get gears(): Gear[] {
+    return this.build.gears;
+  }
+}
+</script>
+
+<style scoped></style>
