b7b2a1326a7ce50344005b1994967e1567e6aaed
TWT <twt2333@outlook.com>
2020-09-10 23:26:03 +0800

    [mod] add Math.floor() for GearCateList.ts

--- src/data/GearCateList.ts
+++ src/data/GearCateList.ts
@@ -13,10 +13,10 @@ export const GearCateList: GearCateEntry[] = [
     name: "SWORD",
     type: "weapon",
     handler: (a: Attribute, g: Gear): Attribute => {
-      a.ATK_PHY_B += g.lvl * g.v(0) * 10;
-      a.ATK_MAG_B += g.lvl * g.v(1) * 10;
-      a.THR_PHY_B += g.lvl * g.v(2);
-      a.HP_STL += (g.lvl / 15 + 10) * g.v(3);
+      a.ATK_PHY_B += Math.floor(g.lvl * g.v(0) * 10);
+      a.ATK_MAG_B += Math.floor(g.lvl * g.v(1) * 10);
+      a.THR_PHY_B += Math.floor(g.lvl * g.v(2));
+      a.HP_STL += Math.floor((g.lvl / 15 + 10) * g.v(3));
       return a;
     }
   },
@@ -24,10 +24,10 @@ export const GearCateList: GearCateEntry[] = [
     name: "SHORT_BOW",
     type: "weapon",
     handler: (a: Attribute, g: Gear): Attribute => {
-      a.ATK_PHY_B += g.lvl * g.v(0) * 10;
-      a.ATK_MAG_B += g.lvl * g.v(1) * 10;
-      a.ATK_SPD_B += g.lvl * g.v(2) * 2;
-      a.HP_STL += (g.lvl / 15 + 10) * g.v(3);
+      a.ATK_PHY_B += Math.floor(g.lvl * g.v(0) * 10);
+      a.ATK_MAG_B += Math.floor(g.lvl * g.v(1) * 10);
+      a.ATK_SPD_B += Math.floor(g.lvl * g.v(2) * 2);
+      a.HP_STL += Math.floor((g.lvl / 15 + 10) * g.v(3));
       return a;
     }
   },
@@ -35,10 +35,10 @@ export const GearCateList: GearCateEntry[] = [
     name: "STAFF",
     type: "weapon",
     handler: (a: Attribute, g: Gear): Attribute => {
-      a.ATK_PHY_B += g.lvl * g.v(0) * 10;
-      a.ATK_MAG_B += g.lvl * g.v(1) * 10;
-      a.THR_MAG_A += (g.lvl / 20 + 5) * g.v(2);
-      a.HP_STL += (g.lvl / 15 + 10) * g.v(3);
+      a.ATK_PHY_B += Math.floor(g.lvl * g.v(0) * 10);
+      a.ATK_MAG_B += Math.floor(g.lvl * g.v(1) * 10);
+      a.THR_MAG_A += Math.floor((g.lvl / 20 + 5) * g.v(2));
+      a.HP_STL += Math.floor((g.lvl / 15 + 10) * g.v(3));
       return a;
     }
   },
@@ -46,10 +46,10 @@ export const GearCateList: GearCateEntry[] = [
     name: "BLADE",
     type: "weapon",
     handler: (a: Attribute, g: Gear): Attribute => {
-      a.ATK_PHY_A += (g.lvl / 5 + 20) * g.v(0);
-      a.ATK_SPD_A += (g.lvl / 5 + 20) * g.v(1);
-      a.THR_CRI_A += (g.lvl / 20 + 10) * g.v(2);
-      a.THR_PHY_A += (g.lvl / 20 + 10) * g.v(3);
+      a.ATK_PHY_A += Math.floor((g.lvl / 5 + 20) * g.v(0));
+      a.ATK_SPD_A += Math.floor((g.lvl / 5 + 20) * g.v(1));
+      a.THR_CRI_A += Math.floor((g.lvl / 20 + 10) * g.v(2));
+      a.THR_PHY_A += Math.floor((g.lvl / 20 + 10) * g.v(3));
       return a;
     }
   },
@@ -57,10 +57,10 @@ export const GearCateList: GearCateEntry[] = [
     name: "ASS_BOW",
     type: "weapon",
     handler: (a: Attribute, g: Gear): Attribute => {
-      a.ATK_PHY_A += (g.lvl / 5 + 30) * g.v(0);
-      a.THR_CRI_A += (g.lvl / 20 + 10) * g.v(1);
-      a.THR_PHY_A += (g.lvl / 20 + 10) * g.v(2);
-      a.THR_PHY_B += g.lvl * g.v(3);
+      a.ATK_PHY_A += Math.floor((g.lvl / 5 + 30) * g.v(0));
+      a.THR_CRI_A += Math.floor((g.lvl / 20 + 10) * g.v(1));
+      a.THR_PHY_A += Math.floor((g.lvl / 20 + 10) * g.v(2));
+      a.THR_PHY_B += Math.floor(g.lvl * g.v(3));
       return a;
     }
   },
@@ -68,10 +68,10 @@ export const GearCateList: GearCateEntry[] = [
     name: "GLOVE",
     type: "hand",
     handler: (a: Attribute, g: Gear): Attribute => {
-      a.ATK_PHY_B += g.lvl * g.v(0) * 10;
-      a.ATK_MAG_B += g.lvl * g.v(1) * 10;
-      a.ATK_SPD_A += g.lvl * g.v(2) * 2;
-      a.HP_B += g.lvl * g.v(3) * 5;
+      a.ATK_PHY_B += Math.floor(g.lvl * g.v(0) * 10);
+      a.ATK_MAG_B += Math.floor(g.lvl * g.v(1) * 10);
+      a.ATK_SPD_A += Math.floor(g.lvl * g.v(2) * 2);
+      a.HP_B += Math.floor(g.lvl * g.v(3) * 5);
       return a;
     }
   },
@@ -79,10 +79,10 @@ export const GearCateList: GearCateEntry[] = [
     name: "BRACELET",
     type: "hand",
     handler: (a: Attribute, g: Gear): Attribute => {
-      a.ATK_MAG_A += (g.lvl / 5 + 1) * g.v(0);
-      a.THR_MAG_A += (g.lvl / 20 + 1) * g.v(1);
-      a.SHD_B += g.lvl * g.v(2) * 13;
-      a.DEF_MAG_B += g.lvl * g.v(3);
+      a.ATK_MAG_A += Math.floor((g.lvl / 5 + 1) * g.v(0));
+      a.THR_MAG_A += Math.floor((g.lvl / 20 + 1) * g.v(1));
+      a.SHD_B += Math.floor(g.lvl * g.v(2) * 13);
+      a.DEF_MAG_B += Math.floor(g.lvl * g.v(3));
       return a;
     }
   },
@@ -90,10 +90,10 @@ export const GearCateList: GearCateEntry[] = [
     name: "PLATE",
     type: "body",
     handler: (a: Attribute, g: Gear): Attribute => {
-      a.HP_B += g.lvl * g.v(0) * 5;
-      a.DEF_PHY_B += g.lvl * g.v(1);
-      a.DEF_MAG_B += g.lvl * g.v(2);
-      a.HP_REG_B += g.lvl * g.v(3) * 2;
+      a.HP_B += Math.floor(g.lvl * g.v(0) * 5);
+      a.DEF_PHY_B += Math.floor(g.lvl * g.v(1));
+      a.DEF_MAG_B += Math.floor(g.lvl * g.v(2));
+      a.HP_REG_B += Math.floor(g.lvl * g.v(3) * 2);
       return a;
     }
   },
@@ -101,10 +101,10 @@ export const GearCateList: GearCateEntry[] = [
     name: "LEATHER",
     type: "body",
     handler: (a: Attribute, g: Gear): Attribute => {
-      a.HP_B += g.lvl * g.v(0) * 10;
-      a.BAR_PHY += g.lvl * g.v(1) * 2;
-      a.BAR_MAG += g.lvl * g.v(2) * 2;
-      a.HP_REG_B += g.lvl * g.v(3) * 2;
+      a.HP_B += Math.floor(g.lvl * g.v(0) * 10);
+      a.BAR_PHY += Math.floor(g.lvl * g.v(1) * 2);
+      a.BAR_MAG += Math.floor(g.lvl * g.v(2) * 2);
+      a.HP_REG_B += Math.floor(g.lvl * g.v(3) * 2);
       return a;
     }
   },
@@ -112,10 +112,10 @@ export const GearCateList: GearCateEntry[] = [
     name: "CLOTH",
     type: "body",
     handler: (a: Attribute, g: Gear): Attribute => {
-      a.HP_B += g.lvl * g.v(0) * 10;
-      a.BAR_PHY += g.lvl * g.v(1) * 2;
-      a.BAR_MAG += g.lvl * g.v(2) * 2;
-      a.HP_REG_B += g.lvl * g.v(3) * 2;
+      a.HP_B += Math.floor(g.lvl * g.v(0) * 10);
+      a.BAR_PHY += Math.floor(g.lvl * g.v(1) * 2);
+      a.BAR_MAG += Math.floor(g.lvl * g.v(2) * 2);
+      a.HP_REG_B += Math.floor(g.lvl * g.v(3) * 2);
       return a;
     }
   },
@@ -123,10 +123,10 @@ export const GearCateList: GearCateEntry[] = [
     name: "CLOAK",
     type: "body",
     handler: (a: Attribute, g: Gear): Attribute => {
-      a.HP_B += g.lvl * g.v(0) * 10;
-      a.SHD_REG_B += g.lvl * g.v(1) * 5;
-      a.SHD_A += (g.lvl / 3 + 20) * g.v(2);
-      a.SHD_B += g.lvl * g.v(3) * 2;
+      a.HP_B += Math.floor(g.lvl * g.v(0) * 10);
+      a.SHD_REG_B += Math.floor(g.lvl * g.v(1) * 5);
+      a.SHD_A += Math.floor((g.lvl / 3 + 20) * g.v(2));
+      a.SHD_B += Math.floor(g.lvl * g.v(3) * 20);
       return a;
     }
   },
@@ -134,10 +134,10 @@ export const GearCateList: GearCateEntry[] = [
     name: "THORN",
     type: "body",
     handler: (a: Attribute, g: Gear): Attribute => {
-      a.HP_A += (g.lvl / 5 + 20) * g.v(0);
-      a.DEF_PHY_A += g.lvl * g.v(1);
-      a.DEF_MAG_A += g.lvl * g.v(2);
-      a.REF += (g.lvl / 15 + 10) * g.v(3);
+      a.HP_A += Math.floor((g.lvl / 5 + 20) * g.v(0));
+      a.DEF_PHY_A += Math.floor(g.lvl * g.v(1));
+      a.DEF_MAG_A += Math.floor(g.lvl * g.v(2));
+      a.REF += Math.floor((g.lvl / 15 + 10) * g.v(3));
       return a;
     }
   },
@@ -145,10 +145,10 @@ export const GearCateList: GearCateEntry[] = [
     name: "SCARF",
     type: "head",
     handler: (a: Attribute, g: Gear): Attribute => {
-      a.HP_B += g.lvl * g.v(0) * 5;
-      a.BAR_PHY += g.lvl * g.v(1) * 2;
-      a.BAR_MAG += g.lvl * g.v(2) * 2;
-      a.HP_REG_B += g.lvl * g.v(3) * 2;
+      a.HP_B += Math.floor(g.lvl * g.v(0) * 5);
+      a.BAR_PHY += Math.floor(g.lvl * g.v(1) * 2);
+      a.BAR_MAG += Math.floor(g.lvl * g.v(2) * 2);
+      a.HP_REG_B += Math.floor(g.lvl * g.v(3) * 2);
       return a;
     }
   },
@@ -156,10 +156,10 @@ export const GearCateList: GearCateEntry[] = [
     name: "ASTRO",
     type: "head",
     handler: (a: Attribute, g: Gear): Attribute => {
-      a.HP_B += g.lvl * g.v(0) * 5;
-      a.SHD_A += (g.lvl / 3 + 1) * g.v(1);
-      a.SHD_B += g.lvl * g.v(2) * 10;
-      a.BAR_PHY += g.lvl * g.v(3) * 2;
+      a.HP_B += Math.floor(g.lvl * g.v(0) * 5);
+      a.SHD_A += Math.floor((g.lvl / 3 + 1) * g.v(1));
+      a.SHD_B += Math.floor(g.lvl * g.v(2) * 10);
+      a.BAR_PHY += Math.floor(g.lvl * g.v(3) * 2);
       return a;
     }
   }
