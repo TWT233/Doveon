71efac481e0f6aafdfd93f3e8471f332208a4d20
TWT <twt2333@outlook.com>
2020-09-10 17:31:10 +0800

    [add] preset gears according to game update

--- src/data/GearCateList.ts
+++ src/data/GearCateList.ts
@@ -21,7 +21,7 @@ export const GearCateList: GearCateEntry[] = [
     }
   },
   {
-    name: "BOW",
+    name: "SHORT_BOW",
     type: "weapon",
     handler: (a: Attribute, g: Gear): Attribute => {
       a.ATK_PHY_B += g.lvl * g.v(0) * 10;
@@ -37,7 +37,7 @@ export const GearCateList: GearCateEntry[] = [
     handler: (a: Attribute, g: Gear): Attribute => {
       a.ATK_PHY_B += g.lvl * g.v(0) * 10;
       a.ATK_MAG_B += g.lvl * g.v(1) * 10;
-      a.THR_MAG_A += (g.lvl / 12 + 5) * g.v(2);
+      a.THR_MAG_A += (g.lvl / 20 + 5) * g.v(2);
       a.HP_STL += (g.lvl / 15 + 10) * g.v(3);
       return a;
     }
@@ -48,8 +48,41 @@ export const GearCateList: GearCateEntry[] = [
     handler: (a: Attribute, g: Gear): Attribute => {
       a.ATK_PHY_A += (g.lvl / 5 + 20) * g.v(0);
       a.ATK_SPD_A += (g.lvl / 5 + 20) * g.v(1);
-      a.THR_CRI_A += (g.lvl / 15 + 10) * g.v(2);
-      a.THR_PHY_A += (g.lvl / 12 + 10) * g.v(3);
+      a.THR_CRI_A += (g.lvl / 20 + 10) * g.v(2);
+      a.THR_PHY_A += (g.lvl / 20 + 10) * g.v(3);
+      return a;
+    }
+  },
+  {
+    name: "ASS_BOW",
+    type: "weapon",
+    handler: (a: Attribute, g: Gear): Attribute => {
+      a.ATK_PHY_A += (g.lvl / 5 + 30) * g.v(0);
+      a.THR_CRI_A += (g.lvl / 20 + 10) * g.v(1);
+      a.THR_PHY_A += (g.lvl / 20 + 10) * g.v(2);
+      a.THR_PHY_B += g.lvl * g.v(3);
+      return a;
+    }
+  },
+  {
+    name: "GLOVE",
+    type: "hand",
+    handler: (a: Attribute, g: Gear): Attribute => {
+      a.ATK_PHY_B += g.lvl * g.v(0) * 10;
+      a.ATK_MAG_B += g.lvl * g.v(1) * 10;
+      a.ATK_SPD_A += g.lvl * g.v(2) * 2;
+      a.HP_B += g.lvl * g.v(3) * 5;
+      return a;
+    }
+  },
+  {
+    name: "BRACELET",
+    type: "hand",
+    handler: (a: Attribute, g: Gear): Attribute => {
+      a.ATK_MAG_A += (g.lvl / 5 + 1) * g.v(0);
+      a.THR_MAG_A += (g.lvl / 20 + 1) * g.v(1);
+      a.SHD_B += g.lvl * g.v(2) * 13;
+      a.DEF_MAG_B += g.lvl * g.v(3);
       return a;
     }
   },
@@ -109,28 +142,6 @@ export const GearCateList: GearCateEntry[] = [
     }
   },
   {
-    name: "GLOVE",
-    type: "hand",
-    handler: (a: Attribute, g: Gear): Attribute => {
-      a.ATK_PHY_B += g.lvl * g.v(0) * 10;
-      a.ATK_MAG_B += g.lvl * g.v(1) * 10;
-      a.ATK_SPD_A += g.lvl * g.v(2) * 2;
-      a.HP_B += g.lvl * g.v(3) * 5;
-      return a;
-    }
-  },
-  {
-    name: "BRACELET",
-    type: "hand",
-    handler: (a: Attribute, g: Gear): Attribute => {
-      a.ATK_MAG_A += (g.lvl / 5 + 1) * g.v(0);
-      a.THR_MAG_A += (g.lvl / 12 + 1) * g.v(1);
-      a.SHD_B += g.lvl * g.v(2) * 10;
-      a.DEF_MAG_B += g.lvl * g.v(3);
-      return a;
-    }
-  },
-  {
     name: "SCARF",
     type: "head",
     handler: (a: Attribute, g: Gear): Attribute => {
@@ -138,6 +149,17 @@ export const GearCateList: GearCateEntry[] = [
       a.BAR_PHY += g.lvl * g.v(1) * 2;
       a.BAR_MAG += g.lvl * g.v(2) * 2;
       a.HP_REG_B += g.lvl * g.v(3) * 2;
+      return a;
+    }
+  },
+  {
+    name: "ASTRO",
+    type: "head",
+    handler: (a: Attribute, g: Gear): Attribute => {
+      a.HP_B += g.lvl * g.v(0) * 5;
+      a.SHD_A += (g.lvl / 3 + 1) * g.v(1);
+      a.SHD_B += g.lvl * g.v(2) * 10;
+      a.BAR_PHY += g.lvl * g.v(3) * 2;
       return a;
     }
   }
