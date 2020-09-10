e82000ee0332028ccf477d5f6a6a8c517f0d5d82
TWT <twt2333@outlook.com>
2020-09-10 14:25:12 +0800

    [add] add a ui component to wrap status table

--- src/components/build/StatusView.vue
+++ src/components/build/StatusView.vue
@@ -1,14 +1,19 @@
 <template>
-  <v-simple-table>
-    <template v-slot:default>
-      <tbody>
-        <tr v-for="item in statusKeys" :key="item">
-          <td>{{ item }}</td>
-          <td>{{ Math.floor(status[item]) }}</td>
-        </tr>
-      </tbody>
-    </template>
-  </v-simple-table>
+  <v-card>
+    <v-card-title>Status</v-card-title>
+    <v-card-text>
+      <v-simple-table>
+        <template v-slot:default>
+          <tbody>
+            <tr v-for="item in statusKeys" :key="item">
+              <td>{{ item }}</td>
+              <td>{{ Math.floor(status[item]) }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script lang="ts">
@@ -17,7 +22,7 @@ import { Status } from "@/mechanism/build/Status";
 
 @Component({})
 export default class StatusView extends Vue {
-  @Prop(Status) status!: Status;
+  @Prop() status!: Status;
 
   get statusKeys(): string[] {
     return Object.keys(this.status);
