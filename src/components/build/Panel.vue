71ec8113471cc37894ede3e6825a39c3631041d8
TWT <twt2333@outlook.com>
2020-09-10 23:32:37 +0800

    [add] Panel.vue: easy tool access for user

---
+++ src/components/build/Panel.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card>
+    <v-card-title>Panel</v-card-title>
+    <v-card-text>
+      <v-container>
+        <v-row>
+          <v-col cols="1">
+            <v-btn color="primary" @click="onSave()">Save</v-btn>
+          </v-col>
+          <v-col cols="1">
+            <v-btn @click="onLoad()">Load</v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class Panel extends Vue {
+  onSave() {
+    this.$emit("panel-save");
+  }
+
+  onLoad() {
+    this.$emit("panel-load");
+  }
+}
+</script>
+
+<style scoped></style>
