e6be4987765a2e32fc8fd7251b3aba0b1d6dbbe3
TWT <twt2333@outlook.com>
2020-09-11 20:52:57 +0800

    [mod] move i18n of GearCate to SingleGearSelect.vue
    but there shall be better approach to achieve this.

--- src/components/build/SingleGearSelect.vue
+++ src/components/build/SingleGearSelect.vue
@@ -1,5 +1,23 @@
 <i18n>
 zh_CN:
+  NONE: 空
+  SWORD: 探险者之剑
+  SHORT_BOW: 探险者短弓
+  STAFF: 探险者短杖
+  BLADE: 狂信者的荣誉之刃
+  ASS_BOW: 反叛者的刺杀弓
+  GLOVE: 探险者手套
+  BRACELET: 命师的传承手环
+  PLATE: 探险者铁甲
+  LEATHER: 探险者皮甲
+  CLOTH: 探险者布甲
+  CLOAK: 旅法师的灵光袍
+  THORN: 战线支撑者的荆棘重甲
+  SCARF: 探险者头巾
+  ASTRO: 占星师的发饰
+</i18n>
+<i18n>
+zh_CN:
   weapon: 武器
   hand: 饰品
   body: 护甲
@@ -71,10 +89,13 @@ export default class SingleGearSelect extends Vue {
   @Prop(Gear) gear!: Gear;
 
   get availableGearList() {
-    const ret = new Array<string>(0);
+    const ret = new Array<{ text: string; value: string }>(0);
     for (const key in GearCateList) {
       if (GearCateList[key].type == this.type) {
-        ret.push(this.$t("GearCate." + GearCateList[key].name).toString());
+        ret.push({
+          text: this.$t(GearCateList[key].name).toString(),
+          value: GearCateList[key].name
+        });
       }
     }
     return ret;
