ca2b730a53ae77476ce08fe3c929b7260ec4e391
TWT <twt2333@outlook.com>
2020-09-11 19:40:18 +0800

    [add] init i18n via vue-i18n

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,25 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  chainWebpack: config => {
+    config.module
+      .rule("i18n")
+      .resourceQuery(/blockType=i18n/)
+      .type("javascript/auto")
+      .use("i18n")
+      .loader("@kazupon/vue-i18n-loader")
+      .end()
+      .use("yaml")
+      .loader("yaml-loader")
+      .end();
+  },
+
+  transpileDependencies: ["vuetify"],
+
+  pluginOptions: {
+    i18n: {
+      locale: "zh_CN",
+      fallbackLocale: "en",
+      localeDir: "data/i18n",
+      enableInSFC: true
+    }
+  }
 };
