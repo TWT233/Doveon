ca896214ea850bd8983d1923151faa311328e98e
TWT <twt2333@outlook.com>
2020-09-11 17:20:45 +0800

    [add] routes/entrance to simulator, add app-bar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
-  <v-app id="app">
+  <v-app>
+    <v-app-bar color="primary" dark app>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Doveon</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text @click="onBuilder()">Builder</v-btn>
+      <v-btn text @click="onSimulator()">Simulator</v-btn>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
     <v-main>
-      <Build></Build>
+      <v-container>
+        <v-row>
+          <v-col>
+            <router-view></router-view>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
@@ -11,5 +30,13 @@ import { Component, Vue } from "vue-property-decorator";
 import Builder from "@/components/build/Builder.vue";
 
 @Component({ components: { Build: Builder } })
-export default class App extends Vue {}
+export default class App extends Vue {
+  onBuilder() {
+    this.$router.push("builder").catch(e => e);
+  }
+
+  onSimulator() {
+    this.$router.push("simulator").catch(e => e);
+  }
+}
 </script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,15 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
+import Builder from "@/components/build/Builder.vue";
+import Simulator from "@/components/simulate/Simulator.vue";
 
 Vue.use(VueRouter);
 
-const routes: Array<RouteConfig> = [];
+const routes: Array<RouteConfig> = [
+  { path: "/", redirect: "/builder" },
+  { path: "/builder", component: Builder },
+  { path: "/simulator", component: Simulator }
+];
 
 const router = new VueRouter({
   routes
