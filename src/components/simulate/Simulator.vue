527550436cdffd49637625e8fc7fa172bf7e26f0
TWT <twt2333@outlook.com>
2020-09-11 17:20:07 +0800

    [add] Simulator.vue: battle simulating

---
+++ src/components/simulate/Simulator.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-card-title>Simulator</v-card-title>
+          <v-card-text>WIP</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class Simulator extends Vue {}
+</script>
+
+<style scoped></style>
