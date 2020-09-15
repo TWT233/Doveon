e9f61b82a936bcb88fbce5ffc568451d3df26339
TWT <twt2333@outlook.com>
2020-09-15 14:32:33 +0800

    [mod] ui adjustments

--- src/components/build/Builder.vue
+++ src/components/build/Builder.vue
@@ -4,21 +4,17 @@
       <v-col lg="3" cols="12">
         <StatusView></StatusView>
       </v-col>
-      <v-col lg="6" cols="12">
-        <GearSelect></GearSelect>
-      </v-col>
-      <v-col lg="3" cols="12">
+      <v-col lg="9" cols="12">
         <v-row>
-          <v-col cols="12"> <PtsEditor></PtsEditor></v-col>
-          <v-col cols="12">
+          <v-col cols="12"> <GearSelect></GearSelect> </v-col>
+          <v-col sm="5" cols="12"> <PtsEditor></PtsEditor></v-col>
+          <!--          <v-col sm="4" cols="12"> <Arsenal></Arsenal> </v-col>-->
+          <v-col sm="3" cols="12">
             <Panel @panel-save="onSave()" @panel-load="onLoad()"></Panel>
           </v-col>
         </v-row>
       </v-col>
     </v-row>
-    <v-snackbar v-model="snackbarShow" :timeout="1000" :bottom="true">
-      {{ snackbarText }}
-    </v-snackbar>
   </v-container>
 </template>
 
@@ -27,40 +23,31 @@ import { Component, Vue } from "vue-property-decorator";
 import GearSelect from "@/components/build/GearSelect.vue";
 import StatusView from "@/components/build/StatusView.vue";
 import { Build } from "@/mechanism/build/Build";
+import Arsenal from "@/components/build/Arsenal.vue";
 import PtsEditor from "@/components/build/PtsEditor.vue";
 import Panel from "@/components/build/Panel.vue";
 
-@Component({ components: { Panel, PtsEditor, GearSelect, StatusView } })
+@Component({
+  components: { Panel, PtsEditor, GearSelect, StatusView, Arsenal }
+})
 export default class Builder extends Vue {
   build: Build = this.$store.state.build;
 
-  snackbarText = "";
-  snackbarShow = false;
-
   onSave() {
     if (!window.localStorage) {
-      this.snackbarText = "LocalStorage not supported";
-      this.snackbarShow = true;
       throw Error("LocalStorage not supported");
     }
-    this.snackbarText = "Saving...";
-    this.snackbarShow = true;
     window.localStorage["build"] = JSON.stringify(this.build);
   }
 
   onLoad() {
     if (!window.localStorage) {
-      this.snackbarText = "LocalStorage not supported";
-      this.snackbarShow = true;
       throw Error("LocalStorage not supported");
     }
 
     if (window.localStorage.getItem("build") === null) {
-      this.snackbarText = "No Data";
-      this.snackbarShow = true;
+      throw Error("no `build` in LocalStorage");
     } else {
-      this.snackbarText = "Loading...";
-      this.snackbarShow = true;
       this.build.load(JSON.parse(window.localStorage["build"]));
     }
   }
