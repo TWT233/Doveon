33cb1e48629f77c4e048a30ca07944d894a2cdd5
TWT <twt2333@outlook.com>
2020-09-17 18:26:08 +0800

    [fix] fix arsenalLoad() logic

--- src/store/index.ts
+++ src/store/index.ts
@@ -13,7 +13,7 @@ export default new Vuex.Store({
   getters: {},
   mutations: {
     arsenalLoad(state, a: ArsenalEntry[]) {
-      state.arsenal = a.slice();
+      a.forEach(e => state.arsenal.push(e));
     },
     arsenalRemove(state, pos: number) {
       if (pos < 0 || state.arsenal.length <= pos) return;
