e1d3cac511b09b883258b7ed14bebc3c62fa85a2
TWT <twt2333@outlook.com>
2020-09-21 21:37:11 +0800

    [add] Action.ts: atom element in battle, one participant do a action

---
+++ src/mechanism/battle/Action.ts
@@ -0,0 +1,44 @@
+import { BattleInfo } from "@/mechanism/battle/BattleInfo";
+import _ from "lodash";
+import { Mob } from "@/mechanism/mob/Mob";
+import { BattleStatus } from "@/mechanism/battle/BattleStatus";
+
+export class Action {
+  a: Mob;
+  b: Mob;
+  before: {
+    a: BattleStatus;
+    b: BattleStatus;
+    battle: BattleInfo;
+  };
+  after: {
+    a: BattleStatus;
+    b: BattleStatus;
+    battle: BattleInfo;
+  };
+
+  constructor(
+    a: Mob,
+    b: Mob,
+    before: { a: BattleStatus; b: BattleStatus; battle: BattleInfo }
+  ) {
+    this.a = a;
+    this.b = b;
+    this.before = before;
+    this.after = _.cloneDeep(before);
+  }
+
+  run() {
+    let spdCounter = this.after.battle.spdCounter;
+    let spdThisAct = Math.min(spdCounter.a, spdCounter.b);
+    spdCounter.a -= spdThisAct;
+    spdCounter.b -= spdThisAct;
+    if (spdCounter.a == 0) {
+      this.b.run(this.after.b, this.after.a, this.after.battle);
+      spdCounter.a = this.after.a.ATK_SPD;
+    } else if (spdCounter.b == 0) {
+      this.a.run(this.after.a, this.after.b, this.after.battle);
+      spdCounter.b = this.after.b.ATK_SPD;
+    }
+  }
+}
