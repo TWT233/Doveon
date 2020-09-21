eee95ea42295fd0b3b8bba0b04422e9bbae699be
TWT <twt2333@outlook.com>
2020-09-21 21:36:00 +0800

    [add] Mob.ts: add interface for Mob

--- src/mechanism/mob/Mob.ts
+++ src/mechanism/mob/Mob.ts
@@ -1,4 +1,22 @@
+import { BattleInfo } from "@/mechanism/battle/BattleInfo";
+import { BattleStatus } from "@/mechanism/battle/BattleStatus";
+
 export interface Mob {
   name: string;
   type: string;
+  skill: {
+    name: string;
+    type: string;
+    handler: (
+      self: BattleStatus,
+      target: BattleStatus,
+      battle: BattleInfo
+    ) => string;
+  }[];
+
+  run(
+    self: BattleStatus,
+    target: BattleStatus,
+    battle: BattleInfo
+  ): { skillIndex: number };
 }
