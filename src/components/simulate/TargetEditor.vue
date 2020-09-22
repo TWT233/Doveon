6c48865128003484e0b14f8b4b28c5c6b171b6cd
TWT <twt2333@outlook.com>
2020-09-22 23:27:22 +0800

    [add] TargetEditor.vue: edit battle target

---
+++ src/components/simulate/TargetEditor.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Target Editor
+    </v-card-title>
+    <v-card-text> </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class TargetEditor extends Vue {}
+</script>
+
+<style scoped></style>
