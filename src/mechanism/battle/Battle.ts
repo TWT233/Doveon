4e9ecf4560269011c2b5961a19868029f2036703
TWT <twt2333@outlook.com>
2020-10-04 23:13:14 +0800

    [add] Battle.ts: describe and record details of a battle

--- src/mechanism/battle/Battle.ts
+++ src/mechanism/battle/Battle.ts
@@ -1,2 +1,63 @@
+import { Action } from "@/mechanism/battle/Action";
+import { BattleInfo } from "@/mechanism/battle/BattleInfo";
+import { NPC } from "@/mechanism/mob/NPC";
+import { PC } from "@/mechanism/mob/PC";
+import { BattleStatus } from "@/mechanism/battle/BattleStatus";
+
 export class Battle {
+  battleLog: Array<Action> = new Array<Action>();
+
+  battleInfo: BattleInfo = new BattleInfo();
+
+  a: PC;
+  b: NPC;
+
+  constructor(a: PC, b: NPC) {
+    this.a = a;
+    this.b = b;
+  }
+
+  run() {
+    const aBS = new BattleStatus(this.a.status);
+    const bBS = new BattleStatus(this.b.status);
+
+    this.battleInfo.spdCounter.a = aBS.ATK_SPD;
+    this.battleInfo.spdCounter.b = bBS.ATK_SPD;
+
+    this.battleLog.push(
+      new Action(this.a, this.b, {
+        a: aBS,
+        b: bBS,
+        battle: this.battleInfo
+      }).run()
+    );
+    // while (!this.isEnd()) {
+    //   this.battleLog.push(
+    //     new Action(
+    //       this.a,
+    //       this.b,
+    //       this.battleLog[this.battleLog.length - 1].after
+    //     ).run()
+    //   );
+    // }
+  }
+
+  next() {
+    if (this.battleLog.length <= 0 || this.isEnd()) return;
+    this.battleLog.push(
+      new Action(
+        this.a,
+        this.b,
+        this.battleLog[this.battleLog.length - 1].after
+      ).run()
+    );
+  }
+
+  isEnd(): boolean {
+    return (
+      this.battleLog[this.battleLog.length - 1].after.a.HP <= 0 ||
+      this.battleLog[this.battleLog.length - 1].after.b.HP <= 0 ||
+      this.battleLog.length > 100
+    );
+  }
 }
