a54be22f753b64ea3ed396601816b33f303938ad
TWT <twt2333@outlook.com>
2020-10-04 23:15:04 +0800

    [add] LogView.vue: show battle log

---
+++ src/components/simulate/LogView.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-card>
+    <v-card-title>
+      LogView
+    </v-card-title>
+    <v-card-text>
+      <template v-if="battleLog != null">
+        <template v-for="(item, i) in battleLog">
+          {{ item.toString() }}<br :key="i" />
+        </template>
+      </template>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { Action } from "@/mechanism/battle/Action";
+
+@Component({})
+export default class LogView extends Vue {
+  @Prop() battleLog!: Array<Action>;
+}
+</script>
+
+<style scoped></style>
