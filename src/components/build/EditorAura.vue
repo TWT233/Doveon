b6448a2fe44e54a528127ca02cc242c1c1f7809e
TWT <twt2333@outlook.com>
2020-10-08 15:31:02 +0800

    [mod] use global aura i18n rather SFC

--- src/components/build/EditorAura.vue
+++ src/components/build/EditorAura.vue
@@ -1,17 +1,7 @@
+<i18n src="@/data/i18n/AuraTrans.yaml" />
 <i18n>
 zh_CN:
   Aura: 光环天赋
-  SHI: 启程之誓
-  XIN: 启程之心
-  FENG: 启程之风
-  BI: 破壁之心
-  MO: 破魔之心
-  SHANG: 伤口恶化
-  SHEN: 精神创伤
-  CI: 铁甲尖刺
-  FEI: 沸血之志
-  BO: 波澜不惊
-  JU: 飓风之力
 </i18n>
 
 <template>
