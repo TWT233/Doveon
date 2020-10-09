c54009d79ac0d09b55224361556656d99746843e
TWT <twt2333@outlook.com>
2020-10-09 23:14:30 +0800

    [mod] change API, add getter/setter

--- src/mechanism/battle/Mob.ts
+++ src/mechanism/battle/Mob.ts
@@ -8,14 +8,46 @@ export type Skill = {
 };
 
 export class Mob {
-  name: string;
-  type: "PC" | "NPC" | "";
-  skills: Skill[] = [];
-  status: DynStatus;
+  private _name: string;
+  private _type: "PC" | "NPC" | "";
+  private _skills: Skill[] = [];
+  private readonly _status: DynStatus;
 
   constructor(name: string, type: "PC" | "NPC" | "", status: DynStatus) {
-    this.name = name;
-    this.type = type;
-    this.status = status;
+    this._name = name;
+    this._type = type;
+    this._status = new DynStatus(status);
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    this._name = value;
+  }
+
+  get type(): "PC" | "NPC" | "" {
+    return this._type;
+  }
+
+  set type(value: "PC" | "NPC" | "") {
+    this._type = value;
+  }
+
+  get skills(): Skill[] {
+    return this._skills;
+  }
+
+  set skills(value: Skill[]) {
+    this._skills = value;
+  }
+
+  get status(): DynStatus {
+    return this._status;
+  }
+
+  set status(value: DynStatus) {
+    this._status.load(value);
   }
 }
