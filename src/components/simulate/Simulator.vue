8a4fb7117f56a3a59f38ebee60f101cfed0c2953
TWT <twt2333@outlook.com>
2020-10-12 01:11:55 +0800

    [mod] integrate EditorMob.vue into Simulator.vue

--- src/components/simulate/Simulator.vue
+++ src/components/simulate/Simulator.vue
@@ -3,14 +3,53 @@ zh_CN:
 </i18n>
 
 <template>
-  <v-container> </v-container>
+  <v-container>
+    <v-row>
+      <v-col cols="4">
+        <v-card>
+          <v-card-title>Player</v-card-title>
+          <v-card-text>
+            <v-row>
+              <v-col><EditorMob v-model="a"/></v-col>
+              <v-col><EditorMob v-model="b"/></v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import EditorMob from "@/components/simulate/EditorMob.vue";
+import { Mob } from "@/mechanism/battle/Mob";
+import { DynStatus } from "@/mechanism/battle/DynStatus";
+import { Status } from "@/mechanism/build/Status";
 
-@Component({})
-export default class Simulator extends Vue {}
+@Component({
+  components: { EditorMob }
+})
+export default class Simulator extends Vue {
+  _a: Mob = new Mob("", "", new DynStatus(new Status()));
+  _b: Mob = new Mob("", "", new DynStatus(new Status()));
+
+  get a() {
+    return this._a;
+  }
+
+  set a(value: Mob) {
+    this._a.load(value);
+  }
+
+  get b() {
+    return this._b;
+  }
+
+  set b(value: Mob) {
+    this._b.load(value);
+  }
+}
 </script>
 
 <style scoped></style>
