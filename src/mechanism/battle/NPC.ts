c5e426a245e9558e11890cdade171ea1de1eaddd
TWT <twt2333@outlook.com>
2020-10-12 00:55:23 +0800

    [mod] add `aura` for NPC
    cuz there would be skills from aura for NPC

--- src/mechanism/battle/NPC.ts
+++ src/mechanism/battle/NPC.ts
@@ -1,15 +1,18 @@
 import { Status } from "@/mechanism/build/Status";
 import { NPCCate } from "@/data/NPCCate";
+import { Aura } from "@/mechanism/build/Aura";
 
 export class NPC {
   type = "";
   lvl = 1;
   p = 1.0;
+  aura = new Aura();
 
-  constructor(type: string, lvl: number, p: number) {
+  constructor(type: string, lvl: number, p: number, aura: Aura) {
     this.type = type;
     this.lvl = lvl;
     this.p = p;
+    this.aura.load(aura);
   }
 
   get status(): Status {
