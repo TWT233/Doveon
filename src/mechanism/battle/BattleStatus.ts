f9c9deeba597b1a9b17ead7e888ad163f43661ca
TWT <twt2333@outlook.com>
2020-10-21 17:40:46 +0800

    [mod] add fields for modifying ATK in action calculation

--- src/mechanism/battle/BattleStatus.ts
+++ src/mechanism/battle/BattleStatus.ts
@@ -1,5 +1,15 @@
 export class BattleStatus {
   spd: number;
+  atkK = {
+    PHY: 1.0,
+    MAG: 1.0,
+    ABS: 1.0
+  };
+  atkC = {
+    PHY: 0,
+    MAG: 0,
+    ABS: 0
+  };
 
   constructor(b: BattleStatus | null = null) {
     this.spd = b?.spd || 0;
