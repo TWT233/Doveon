09fc9a295fa70fdaa47d4957544386215124f262
TWT <twt2333@outlook.com>
2020-10-21 17:38:08 +0800

    [add] EditorMobStatus.vue: build mob from status data
    WIP

---
+++ src/components/simulate/EditorMobStatus.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-textarea></v-textarea>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { Mob } from "@/mechanism/battle/Mob";
+import { Status } from "@/mechanism/build/Status";
+
+@Component({})
+export default class EditorMobStatus extends Vue {
+  getMob(): Mob {
+    return new Mob("", "PC", new Status());
+  }
+}
+</script>
+
+<style scoped></style>
