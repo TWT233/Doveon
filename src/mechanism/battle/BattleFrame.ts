a094063013306a69d1bf90d62ff9becbb82b37a9
TWT <twt2333@outlook.com>
2020-10-24 23:03:55 +0800

    [add] static BattleFrame.init()
    used in a new Battle, to gen a brand new BF with given init BattleStatus

--- src/mechanism/battle/BattleFrame.ts
+++ src/mechanism/battle/BattleFrame.ts
@@ -11,4 +11,14 @@ export class BattleFrame {
     this.b = new DynStatus(b?.b);
     this.e = new BattleStatus(b?.e);
   }
+
+  static init(a: DynStatus, b: DynStatus, e: BattleStatus): BattleFrame {
+    const ret: BattleFrame = {
+      a: new DynStatus(a),
+      b: new DynStatus(b),
+      e: e
+    };
+    ret.e.spd = ret.a.ATK_SPD - ret.b.ATK_SPD;
+    return ret;
+  }
 }
