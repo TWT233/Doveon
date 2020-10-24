f629eb979c9632fb8d1bb457545be9fcd9e5a572
TWT <twt2333@outlook.com>
2020-10-24 23:04:30 +0800

    [add] Battle.run()
    run a battle

--- src/mechanism/battle/Battle.ts
+++ src/mechanism/battle/Battle.ts
@@ -1,13 +1,48 @@
 import { Mob } from "@/mechanism/battle/Mob";
 import { Action } from "@/mechanism/battle/Action";
+import { BattleStatus } from "@/mechanism/battle/BattleStatus";
+import { BattleFrame } from "@/mechanism/battle/BattleFrame";
 
 export class Battle {
   a: Mob;
   b: Mob;
+  e: BattleStatus;
   log: Array<Action> = new Array<Action>();
 
-  constructor(a: Mob, b: Mob) {
+  constructor(a: Mob, b: Mob, e: BattleStatus) {
     this.a = a;
     this.b = b;
+    this.e = e;
+  }
+
+  run(n = 100) {
+    this.log.splice(0, this.log.length);
+    for (; !this.isEnd() && n > 0; n--) {
+      console.log("step:" + n);
+      this.stepOver();
+    }
+  }
+
+  stepOver(): Battle {
+    this.log.push(
+      new Action(
+        this.a,
+        this.b,
+        this.log.length == 0
+          ? BattleFrame.init(this.a.status, this.b.status, this.e)
+          : this.log[this.log.length - 1].after
+      ).exec()
+    );
+
+    return this;
+  }
+
+  isEnd(): boolean {
+    return (
+      this.log.length > 0 &&
+      (this.log[this.log.length - 1].after.a.HP <= 0 ||
+        this.log[this.log.length - 1].after.b.HP <= 0 ||
+        this.log.length >= 100)
+    );
   }
 }
