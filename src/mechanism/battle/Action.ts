21a465c964d20d3f04d1150d13806a739ced8867
TWT <twt2333@outlook.com>
2020-10-24 23:02:55 +0800

    [add] Action.exec()
    exec an Action

--- src/mechanism/battle/Action.ts
+++ src/mechanism/battle/Action.ts
@@ -13,4 +13,43 @@ export class Action {
     this.before = before;
     this.after = new BattleFrame(this.before);
   }
+
+  exec(): Action {
+    const s = this.onSPDGetExec();
+
+    this.onPassiveBefore();
+
+    if (s == "a") {
+      this.a.skills[0].run(this.after, s);
+    } else if (s == "b") {
+      this.b.skills[0].run(this.after, s);
+    }
+
+    this.onSPDRefresh();
+
+    return this;
+  }
+
+  onSPDGetExec() {
+    return this.after.e.spd >= 0 ? "a" : "b";
+  }
+
+  onPassiveBefore() {
+    this.a.skills.forEach(e => {
+      if (e.type == "passive" && e.isBefore) {
+        e.run(this.after, "a");
+      }
+    });
+    this.b.skills.forEach(e => {
+      if (e.type == "passive" && e.isBefore) {
+        e.run(this.after, "b");
+      }
+    });
+  }
+
+  onSPDRefresh() {
+    this.onSPDGetExec() == "a"
+      ? (this.after.e.spd -= this.after.b.ATK_SPD)
+      : (this.after.e.spd += this.after.a.ATK_SPD);
+  }
 }
