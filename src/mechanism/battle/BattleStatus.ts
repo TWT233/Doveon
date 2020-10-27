2333903a8c632d11b4f5928942ffcefe204a6892
TWT <twt2333@outlook.com>
2020-10-28 00:29:43 +0800

    [add] field atkB to modify dmg

--- src/mechanism/battle/BattleStatus.ts
+++ src/mechanism/battle/BattleStatus.ts
@@ -1,15 +1,23 @@
 export class BattleStatus {
   spd: number;
+  // calculate formula:
+  //    dmg = atkK.* x (ATK_* + atkB.*) + atkC.*
   atkK = {
     PHY: 1.0,
     MAG: 1.0,
     ABS: 1.0
+  };
+  atkB = {
+    PHY: 0,
+    MAG: 0,
+    ABS: 0
   };
   atkC = {
     PHY: 0,
     MAG: 0,
     ABS: 0
   };
+  isCri = false;
 
   constructor(b: BattleStatus | null = null) {
     this.spd = b?.spd || 0;
