0620834c1d458cb8cf7afc2053aa9457bcc78aca
TWT <twt2333@outlook.com>
2020-11-17 23:32:54 +0800

    feat: finish framework for action.exec()

--- src/mechanism/battle/Action.ts
+++ src/mechanism/battle/Action.ts
@@ -1,6 +1,6 @@
 import { Mob } from "@/mechanism/battle/Mob";
 import { BattleFrame } from "@/mechanism/battle/BattleFrame";
-import { ATK } from "@/utils/helper";
+import { dealDMG } from "@/utils/helper";
 
 export class Action {
   a: Mob;
@@ -18,17 +18,23 @@ export class Action {
   exec(): Action {
     const s = this.getExec();
 
+    this.after.e[s].PA = this[s].status.ATK_PHY;
+    this.after.e[s].MA = this[s].status.ATK_MAG;
+    this.after.e[s].AA = this[s].status.ATK_ABS;
+
     this.execBeforeCrit(s);
     this.diceCriAndSki(s);
     this.execActive(s);
     this.diceMagCri(s);
 
     this.beforeATK(s);
-    ATK(this.after, s);
+    dealDMG(this.after, s, "MAG", this.after.e[s].MA);
+    dealDMG(this.after, s, "PHY", this.after.e[s].PA);
+    dealDMG(this.after, s, "ABS", this.after.e[s].AA);
     this.afterATK(s);
 
     this.RFL(s);
-    this.refreshSPD();
+    this.refreshSPD(s);
     return this;
   }
 
@@ -38,9 +44,7 @@ export class Action {
 
   execBeforeCrit(s: "a" | "b") {
     this[s].skills.forEach(e => {
-      if (e.type == "beforeCrit") {
-        e.run(this.after, "a");
-      }
+      if (e.type == "beforeCrit") e.run(this.after, "a");
     });
   }
 
@@ -51,43 +55,56 @@ export class Action {
 
   execActive(s: "a" | "b") {
     this[s].skills.forEach(e => {
-      if (e.type == "active") {
-        e.run(this.after, "a");
-      }
+      if (e.type == "active") e.run(this.after, "a");
     });
   }
 
   diceMagCri(s: "a" | "b") {
     this.after.e.isMC = Math.random() < 0.2;
     this[s].skills.forEach(e => {
-      if (e.type == "onMC") {
-        e.run(this.after, "a");
-      }
+      if (e.type == "onMC") e.run(this.after, "a");
     });
   }
 
   beforeATK(s: "a" | "b") {
     this[s].skills.forEach(e => {
-      if (e.type == "onMC") {
-        e.run(this.after, "a");
-      }
+      if (e.type == "beforeATK") e.run(this.after, "a");
     });
   }
 
   afterATK(s: "a" | "b") {
+    this.after.e.RFL +=
+      (this[s == "a" ? "b" : "a"].status.REF / 100) *
+      (this.after.e[s].PA + this.after.e[s].MA * 0.7 + this.after.e[s].AA);
+
     this[s].skills.forEach(e => {
-      if (e.type == "afterATK") {
-        e.run(this.after, "a");
-      }
+      if (e.type == "afterATK") e.run(this.after, "a");
     });
   }
 
   RFL(s: "a" | "b") {
-    console.log("RFL not implemented");
+    dealDMG(this.after, s == "a" ? "b" : "a", "MAG", this.after.e.RFL);
+
+    this.after.a.HP = Math.min(
+      this.after.a.HP - this.after.e.a.HD + this.after.e.a.HR,
+      this.after.a.origin.HP
+    );
+    this.after.a.SHD = Math.min(
+      this.after.a.SHD - this.after.e.a.SD + this.after.e.a.SR,
+      this.after.a.origin.SHD
+    );
+    this.after.b.HP = Math.min(
+      this.after.b.HP - this.after.e.b.HD + this.after.e.b.HR,
+      this.after.b.origin.HP
+    );
+    this.after.b.SHD = Math.min(
+      this.after.b.SHD - this.after.e.b.SD + this.after.e.b.SR,
+      this.after.b.origin.SHD
+    );
   }
 
-  refreshSPD() {
-    this.getExec() == "a"
+  refreshSPD(s: "a" | "b") {
+    s == "a"
       ? (this.after.e.spd -= this.after.b.ATK_SPD)
       : (this.after.e.spd += this.after.a.ATK_SPD);
   }
