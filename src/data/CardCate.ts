e20e2ddac2cf0718b80c3f61643f33e00bfa47c6
TWT <twt2333@outlook.com>
2020-11-17 17:10:32 +0800

    lint

--- src/data/CardCate.ts
+++ src/data/CardCate.ts
@@ -1,11 +1,8 @@
 import { Skill } from "@/mechanism/battle/Skill";
-import { normalATK } from "@/utils/helper";
 
 export type CardCateEntry = {
   name: string;
   skills: Skill[];
 };
 
-export const CardCate: CardCateEntry[] = [
-  { name: "MENG", skills: [normalATK] }
-];
+export const CardCate: CardCateEntry[] = [{ name: "MENG", skills: [] }];
