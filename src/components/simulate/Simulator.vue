ea217ab2966d8e4b5af74485a2e3a399d90722af
TWT <twt2333@outlook.com>
2020-11-17 17:12:39 +0800

    ui: integrate new ui components

--- src/components/simulate/Simulator.vue
+++ src/components/simulate/Simulator.vue
@@ -9,7 +9,14 @@
               <v-col><EditorMob v-model="a"/></v-col>
               <v-col><EditorMob v-model="b"/></v-col>
             </v-row>
+            <v-row dense>
+              <v-col><ViewBattleLog :value="battle.log" /> </v-col>
+            </v-row>
           </v-card-text>
+          <v-card-actions>
+            <v-btn @click.stop="battle.run()">Run</v-btn>
+            <v-btn @click="load()">Load</v-btn>
+          </v-card-actions>
         </v-card>
       </v-col>
     </v-row>
@@ -21,28 +28,38 @@ import { Component, Vue } from "vue-property-decorator";
 import EditorMob from "@/components/simulate/EditorMob.vue";
 import { Mob } from "@/mechanism/battle/Mob";
 import { Status } from "@/mechanism/build/Status";
+import { Battle } from "@/mechanism/battle/Battle";
+import { BattleStatus } from "@/mechanism/battle/BattleStatus";
+import ViewBattleLog from "@/components/simulate/ViewBattleLog.vue";
 
 @Component({
-  components: { EditorMob }
+  components: { ViewBattleLog, EditorMob }
 })
 export default class Simulator extends Vue {
-  private i_a = new Mob("", "", new Status());
-  private i_b = new Mob("", "", new Status());
+  battle: Battle = new Battle(
+    new Mob("", "", new Status()),
+    new Mob("", "", new Status()),
+    new BattleStatus()
+  );
 
   get a() {
-    return this.i_a;
+    return this.battle.a;
   }
 
   set a(value: Mob) {
-    this.i_a.load(value);
+    this.battle.a.load(value);
   }
 
   get b() {
-    return this.i_b;
+    return this.battle.b;
   }
 
   set b(value: Mob) {
-    this.i_b.load(value);
+    this.battle.b.load(value);
+  }
+
+  load() {
+    this.$store.commit("setBuild", JSON.parse(window.localStorage["build"]));
   }
 }
 </script>
