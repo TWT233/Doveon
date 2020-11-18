212e30aa72cc824d18ad26554e3f96f400e6962b
TWT <twt2333@outlook.com>
2020-11-18 21:31:03 +0800

    feat: `stat` skill: exec only once

--- src/mechanism/battle/Skill.ts
+++ src/mechanism/battle/Skill.ts
@@ -1,7 +1,14 @@
-import {BattleFrame} from "@/mechanism/battle/BattleFrame";
+import { BattleFrame } from "@/mechanism/battle/BattleFrame";
 
 export type Skill = {
   name: string;
-  type: "beforeCrit" | "onCrit" | "active" | "onMC" | "beforeATK" | "afterATK";
+  type:
+    | "stat"
+    | "beforeCrit"
+    | "onCrit"
+    | "active"
+    | "onMC"
+    | "beforeATK"
+    | "afterATK";
   run: (b: BattleFrame, s: "a" | "b") => BattleFrame;
 };
